e69f6794630c4db2de04788660f429f0f0c039f6
Shan Amin <robin_zill@yahoo.com>
2024-09-16 00:02:10 +0500

    cartSlice and reducer

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { apiSlice } from "./slices/apiSlice";
+import cartSliceReducer from "./slices/cartSlice"; // we are able to do this because cartSlice.reducer is the default export so we can use any name
 
 const store = configureStore({
   reducer: {
     [apiSlice.reducerPath]: apiSlice.reducer,
+    cart: cartSliceReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(apiSlice.middleware),
