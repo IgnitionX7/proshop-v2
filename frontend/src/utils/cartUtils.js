3ec5fe6817947487381236a473df2c3408add6c1
Shan Amin <robin_zill@yahoo.com>
2024-09-16 22:48:06 +0500

    cleaned cartSlice and added cartUtils

---
+++ frontend/src/utils/cartUtils.js
@@ -0,0 +1,22 @@
+export const addDecimals = (num) => {
+  return (Math.round(num * 100) / 100).toFixed(2);
+};
+
+export const updateCart = (state) => {
+  //calculate the item price
+  state.itemsPrice = addDecimals(
+    state.cartItems.reduce((acc, item) => acc + item.price * item.qty, 0)
+  );
+  //calculate the shipping price (it order is over 100$ then free, else $10 shipping)
+  state.shippingPrice = addDecimals(state.itemsPrice >= 100 ? 0 : 10);
+  //calculate the tax price (15% tax)
+  state.taxPrice = addDecimals(Number(0.15 * state.itemsPrice).toFixed(2));
+  //calculate the total price
+  state.totalPrice = (
+    Number(state.itemsPrice) +
+    Number(state.shippingPrice) +
+    Number(state.taxPrice)
+  ).toFixed(2);
+  localStorage.setItem("cart", JSON.stringify(state));
+  return state;
+};
