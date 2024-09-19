bbc3ee2e623cd4a71c4f46aa3e15b3e225095c52
Shan Amin <robin_zill@yahoo.com>
2024-09-19 20:10:31 +0500

    authSlice , usersApiSlice and LoginScreen

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { apiSlice } from "./slices/apiSlice";
 import cartSliceReducer from "./slices/cartSlice"; // we are able to do this because cartSlice.reducer is the default export so we can use any name
+import authSliceReducer from "./slices/authSlice";
 
 const store = configureStore({
   reducer: {
     [apiSlice.reducerPath]: apiSlice.reducer,
     cart: cartSliceReducer,
+    auth: authSliceReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(apiSlice.middleware),
