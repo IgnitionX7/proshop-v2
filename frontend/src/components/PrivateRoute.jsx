aedbaa85ea5346b70dcd27eb7e67a5760e4743a2
Shan Amin <robin_zill@yahoo.com>
2024-09-20 22:56:45 +0500

    CheckoutSteps added

--- frontend/src/components/PrivateRoute.jsx
+++ frontend/src/components/PrivateRoute.jsx
@@ -1,7 +1,12 @@
+import { Outlet, Navigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+
 import React from "react";
 
 const PrivateRoute = () => {
-  return <div>PrivateRoute</div>;
+  const { userInfo } = useSelector((state) => state.auth);
+
+  return userInfo ? <Outlet /> : <Navigate to="/login" replace />;
 };
 
 export default PrivateRoute;
