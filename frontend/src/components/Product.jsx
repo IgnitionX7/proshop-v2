0191cc591162c4e17b520a2259ece50c83e616ac
Shan Amin <robin_zill@yahoo.com>
2024-09-25 15:45:22 +0500

    fixed image sizing issue

--- frontend/src/components/Product.jsx
+++ frontend/src/components/Product.jsx
@@ -7,7 +7,11 @@ const Product = ({ product }) => {
   return (
     <Card className="my-3 p-3 rounded">
       <Link to={`/product/${product._id}`}>
-        <Card.Img src={product.image} variant="top" />
+        <Card.Img
+          src={product.image}
+          variant="top"
+          style={{ objectFit: "cover", height: "300px", width: "100%" }}
+        />
       </Link>
       <Card.Body>
         <Link to={`/product/${product._id}`}>
